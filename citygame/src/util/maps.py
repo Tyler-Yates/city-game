dd7000751d59354af03537f2a4530e8120596f1d
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-21 16:23:31 -0500

    Maps

--- citygame/src/util/maps.py
+++ citygame/src/util/maps.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 import noise
@@ -16,10 +17,9 @@ def _generate_square_gradient(width: int, height: int) -> ndarray:
             gradient_matrix[x][y] = max(abs(x - center_x), abs(y - center_y))
 
     normalized_gradient_matrix = gradient_matrix / numpy.max(gradient_matrix)
-    print(normalized_gradient_matrix)
 
-    image = Image.fromarray((normalized_gradient_matrix * 255).astype("uint8"), "L")
-    image.show()
+    # image = Image.fromarray((normalized_gradient_matrix * 255).astype("uint8"), "L")
+    # image.show()
 
     return normalized_gradient_matrix
 
@@ -28,7 +28,7 @@ def _generate_noise(width: int, height: int) -> ndarray:
     noise_matrix = numpy.zeros((width, height))
 
     scale = 100.0
-    octaves = 6
+    octaves = int(math.log(width, 2))
     frequency = 0.5
     amplitude = 2.0
     repeat = 1024
@@ -64,37 +64,55 @@ def add_color(world):
     snow = [255, 250, 250]
     mountain = [139, 137, 137]
 
-    water_threshold = -0.3
+    water_threshold = -0.9
+    max_height = numpy.max(world)
 
     color_world = numpy.zeros(world.shape + (3,))
     for i in range(world.shape[0]):
         for j in range(world.shape[1]):
+
+            # Default type
+            color_world[i][j] = darkgreen
+
             if world[i][j] < water_threshold - 0.25:
                 color_world[i][j] = deep_water
             elif world[i][j] < water_threshold:
                 color_world[i][j] = shallow_water
-            elif world[i][j] < water_threshold + 0.025:
+            elif world[i][j] < water_threshold + 0.05:
                 color_world[i][j] = beach
             elif world[i][j] < water_threshold + 0.25:
                 color_world[i][j] = green
-            elif world[i][j] < water_threshold + 0.6:
-                color_world[i][j] = darkgreen
-            elif world[i][j] < 0.7:
-                color_world[i][j] = mountain
-            else:
+
+            if world[i][j] > max_height - 0.2:
                 color_world[i][j] = snow
+            elif world[i][j] > max_height - 0.5:
+                color_world[i][j] = mountain
 
     return color_world
 
 
+def _apply_gradient(noise_matrix: ndarray, gradient_matrix: ndarray) -> ndarray:
+    resulting_matrix = numpy.zeros_like(noise_matrix)
+
+    for x in range(noise_matrix.shape[0]):
+        for y in range(noise_matrix.shape[1]):
+            resulting_matrix[x][y] = noise_matrix[x][y] - (gradient_matrix[x][y] * 2)
+
+    return resulting_matrix
+
+
 def generate_world(width: int, height: int):
     noise_matrix = _generate_noise(width, height)
+    square_gradient_matrix = _generate_square_gradient(width, height)
 
-    color_world = add_color(noise_matrix)
+    gradient_applied_matrix = _apply_gradient(noise_matrix, square_gradient_matrix)
+
+    color_world = add_color(gradient_applied_matrix)
 
     image = Image.fromarray(color_world.astype("uint8"), "RGB")
     image.show()
 
 
 if __name__ == "__main__":
-    _generate_square_gradient(1024, 1024)
+    size = 1024
+    generate_world(size, size)
