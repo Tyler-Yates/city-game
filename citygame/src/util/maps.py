bdf473733d42845cb75b87c3e0e058fbd64ae891
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-21 15:35:44 -0500

    Map generation improvements

--- citygame/src/util/maps.py
+++ citygame/src/util/maps.py
@@ -20,7 +20,7 @@ def _generate_noise(width: int, height: int) -> ndarray:
     for x in range(width):
         for y in range(height):
 
-            noise_matrix[x][y] = noise.pnoise2(
+            noise_matrix[x][y] = noise._simplex.noise2(
                 x / scale,
                 y / scale,
                 octaves=octaves,
@@ -31,29 +31,35 @@ def _generate_noise(width: int, height: int) -> ndarray:
                 base=base,
             )
 
-    return noise_matrix
+    # Normalize between -1 and 1
+    normalized_noise_matrix = 2.0 * (noise_matrix - numpy.min(noise_matrix)) / numpy.ptp(noise_matrix) - 1
+
+    return normalized_noise_matrix
 
 
 def add_color(world):
-    blue = [0, 0, 0]
+    deep_water = [0, 62, 173]
+    shallow_water = [9, 82, 200]
     green = [34, 139, 34]
     darkgreen = [0, 100, 0]
     beach = [238, 214, 175]
     snow = [255, 250, 250]
     mountain = [139, 137, 137]
 
-    threshold = -0.2
+    water_threshold = -0.3
 
     color_world = numpy.zeros(world.shape + (3,))
     for i in range(world.shape[0]):
         for j in range(world.shape[1]):
-            if world[i][j] < threshold:
-                color_world[i][j] = blue
-            elif world[i][j] < threshold + 0.025:
+            if world[i][j] < water_threshold - 0.25:
+                color_world[i][j] = deep_water
+            elif world[i][j] < water_threshold:
+                color_world[i][j] = shallow_water
+            elif world[i][j] < water_threshold + 0.025:
                 color_world[i][j] = beach
-            elif world[i][j] < threshold + 0.25:
+            elif world[i][j] < water_threshold + 0.25:
                 color_world[i][j] = green
-            elif world[i][j] < threshold + 0.6:
+            elif world[i][j] < water_threshold + 0.6:
                 color_world[i][j] = darkgreen
             elif world[i][j] < 0.7:
                 color_world[i][j] = mountain
