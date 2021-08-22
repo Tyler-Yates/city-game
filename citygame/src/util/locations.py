c2edeabfeb279fee2366747b050de9aa38157774
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-22 17:51:11 -0500

    City placement improvements

--- citygame/src/util/locations.py
+++ citygame/src/util/locations.py
@@ -72,28 +72,41 @@ def place_locations(map_tiles: ndarray) -> ndarray:
     closest_valid_starting_point = _find_closest_valid_position(starting_point, map_tiles)
     locations = [closest_valid_starting_point]
 
-    distance_between_cities = 50
+    distance_to_new_city = 50
+    minimum_distance_between_cities = 30
+    max_angle_iterations = 360
+    angle_increment = 2.0 * math.pi / max_angle_iterations
 
-    for i in range(20):
+    num_cities = 100
+    for i in range(num_cities):
         location = random.choice(locations)
 
-        for k in range(100):
-            angle = 2 * math.pi * random.random()
-            new_location_x = int(location[0] + math.cos(angle) * distance_between_cities)
-            new_location_y = int(location[1] + math.sin(angle) * distance_between_cities)
+        angle = 2.0 * math.pi * random.random()
+        for k in range(max_angle_iterations):
+            angle = (angle + angle_increment) % (2.0 * math.pi)
+            new_location_x = int(location[0] + math.cos(angle) * distance_to_new_city)
+            new_location_y = int(location[1] - math.sin(angle) * distance_to_new_city)
             new_location = (new_location_x, new_location_y)
 
-            if _location_is_valid(new_location, locations, map_tiles, distance_between_cities):
+            if _location_is_valid(new_location, locations, map_tiles, minimum_distance_between_cities):
+                print(f"Angle {angle} - Staring location {location} - New location {new_location}")
                 locations.append(new_location)
+                break
 
     for location in locations:
         map_tiles_with_cities[location[0]][location[1]] = 99
+        map_tiles_with_cities[location[0]][location[1] + 1] = 99
+        map_tiles_with_cities[location[0]][location[1] - 1] = 99
+        map_tiles_with_cities[location[0] + 1][location[1]] = 99
+        map_tiles_with_cities[location[0] - 1][location[1]] = 99
+
+    print(f"Cities: {len(locations)}")
 
     return map_tiles_with_cities
 
 
 def main():
-    size = 500
+    size = 1024
     map_tiles = generate_map(size, size)
     map_tiles_with_cities = place_locations(map_tiles)
 
