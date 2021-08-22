8762e7603e842bc4520e37b5cd886475ed7cba2c
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-22 15:30:33 -0500

    Comments and better code format

--- citygame/src/util/maps.py
+++ citygame/src/util/maps.py
@@ -17,11 +17,16 @@ def _generate_square_gradient(width: int, height: int) -> ndarray:
     center_y = height // 2
     gradient_matrix = numpy.zeros((width, height))
 
+    # Generate a square gradient by using the maximum of the horizontal and vertical distance from the center.
     for x in range(width):
         for y in range(height):
             gradient_matrix[x][y] = max(abs(x - center_x), abs(y - center_y))
 
+    # Normalize the gradient values as floats between [0, 1]
     normalized_gradient_matrix = gradient_matrix / numpy.max(gradient_matrix)
+
+    # Square each value in the gradient so that the center of the map has very low values but the outer edges maintain
+    # values close to 1.
     normalized_gradient_matrix = numpy.square(normalized_gradient_matrix)
 
     # image = Image.fromarray((normalized_gradient_matrix * 255).astype("uint8"), "L")
@@ -39,6 +44,8 @@ def _generate_noise(width: int, height: int) -> ndarray:
     amplitude = 2.0
     repeat = 1048576
 
+    # We need to ensure that we do not exceed the repeat value factoring in scale or the map could have repeated
+    # sections which look odd.
     base = random.randint(0, int(repeat / scale))
     LOG.info(f"Map seed: {base}")
 
@@ -55,37 +62,45 @@ def _generate_noise(width: int, height: int) -> ndarray:
                 base=base,
             )
 
-    # Normalize between -1 and 1
+    # Normalize the matrix values as floats between [-1, 1]
     normalized_noise_matrix = 2.0 * (noise_matrix - numpy.min(noise_matrix)) / numpy.ptp(noise_matrix) - 1
 
     return normalized_noise_matrix
 
 
-def _calculate_tiles(noise_matrix: ndarray) -> ndarray:
-    water_threshold = -0.9
-    max_height = numpy.max(noise_matrix)
-
+def _calculate_tiles(noise_matrix: ndarray, water_threshold: float = -0.9) -> ndarray:
     map_tiles = numpy.zeros(noise_matrix.shape)
 
+    # Calculate the maximum height as a reference point for the higher-level terrain
+    max_height = numpy.max(noise_matrix)
+
+    # Calculate tile threshold values once so we do not do it for every tile
+    deep_water_threshold = water_threshold - 0.25
+    beach_threshold = water_threshold + 0.05
+    grassland_threshold = water_threshold + 0.25
+    snow_threshold = max_height - 0.3
+    mountain_threshold = max_height - 0.6
+
     for x in range(noise_matrix.shape[0]):
-        for j in range(noise_matrix.shape[1]):
-
+        for y in range(noise_matrix.shape[1]):
             # Default type
-            map_tiles[x][j] = MapTile.FOREST.value
+            map_tiles[x][y] = MapTile.FOREST.value
 
-            if noise_matrix[x][j] < water_threshold - 0.25:
-                map_tiles[x][j] = MapTile.DEEP_WATER.value
-            elif noise_matrix[x][j] < water_threshold:
-                map_tiles[x][j] = MapTile.SHALLOW_WATER.value
-            elif noise_matrix[x][j] < water_threshold + 0.05:
-                map_tiles[x][j] = MapTile.BEACH.value
-            elif noise_matrix[x][j] < water_threshold + 0.25:
-                map_tiles[x][j] = MapTile.GRASSLAND.value
+            # Lower-level terrain based on difference from the water threshold
+            if noise_matrix[x][y] < deep_water_threshold:
+                map_tiles[x][y] = MapTile.DEEP_WATER.value
+            elif noise_matrix[x][y] < water_threshold:
+                map_tiles[x][y] = MapTile.SHALLOW_WATER.value
+            elif noise_matrix[x][y] < beach_threshold:
+                map_tiles[x][y] = MapTile.BEACH.value
+            elif noise_matrix[x][y] < grassland_threshold:
+                map_tiles[x][y] = MapTile.GRASSLAND.value
 
-            if noise_matrix[x][j] > max_height - 0.3:
-                map_tiles[x][j] = MapTile.SNOW.value
-            elif noise_matrix[x][j] > max_height - 0.6:
-                map_tiles[x][j] = MapTile.MOUNTAIN.value
+            # Higher level terrain based on difference from the maximum height
+            if noise_matrix[x][y] > snow_threshold:
+                map_tiles[x][y] = MapTile.SNOW.value
+            elif noise_matrix[x][y] > mountain_threshold:
+                map_tiles[x][y] = MapTile.MOUNTAIN.value
 
     map_tiles = map_tiles.astype(int)
     return map_tiles
@@ -96,6 +111,7 @@ def _apply_gradient(noise_matrix: ndarray, gradient_matrix: ndarray) -> ndarray:
 
     for x in range(noise_matrix.shape[0]):
         for y in range(noise_matrix.shape[1]):
+            # Multiply by 2 to ensure the outer edges have very low values below the water threshold
             resulting_matrix[x][y] = noise_matrix[x][y] - (gradient_matrix[x][y] * 2)
 
     return resulting_matrix
@@ -119,7 +135,7 @@ def generate_map(width: int, height: int) -> ndarray:
     return map_tiles
 
 
-if __name__ == "__main__":
+def main():
     logging.basicConfig(level=logging.INFO)
     size = 1024
     map_tiles = generate_map(size, size)
@@ -131,3 +147,7 @@ if __name__ == "__main__":
 
     image = Image.fromarray(rgb_value_matrix.astype("uint8"), "RGB")
     image.show()
+
+
+if __name__ == "__main__":
+    main()
