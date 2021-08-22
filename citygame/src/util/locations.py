2bd0eb70d3e6c1171703cd983ff4008ba1650491
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-22 18:33:44 -0500

    Better comments

--- citygame/src/util/locations.py
+++ citygame/src/util/locations.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import random
 
@@ -7,6 +8,8 @@ from numpy import ndarray
 
 from citygame.src.util.map_tile import MapTile
 from citygame.src.util.maps import generate_map
+
+LOG = logging.getLogger("maps")
 
 
 def _find_closest_valid_position(starting_location, map_tiles) -> tuple[int, int]:
@@ -57,17 +60,15 @@ def _location_is_on_valid_tile(location, map_tiles: ndarray) -> bool:
 def _location_is_valid(
     location, existing_locations, map_tiles: ndarray, minimum_distance_between_locations: int
 ) -> bool:
-    return _location_is_on_valid_tile(location, map_tiles) and (
-        not _location_is_too_close_to_existing_locations(
-            location, existing_locations, minimum_distance_between_locations
-        )
+    tile_valid = _location_is_on_valid_tile(location, map_tiles)
+    distance_valid = not _location_is_too_close_to_existing_locations(
+        location, existing_locations, minimum_distance_between_locations
     )
+    return tile_valid and distance_valid
 
 
-def place_locations(map_tiles: ndarray) -> ndarray:
-    map_tiles_with_cities = numpy.matrix.copy(map_tiles)
-
-    # Add an initial seed location close to the center of the map
+def calculate_locations(map_tiles: ndarray) -> list[tuple[int, int]]:
+    # Add an initial seed location close to the center of the map.
     starting_point = (map_tiles.shape[0] // 2, map_tiles.shape[1] // 2)
     closest_valid_starting_point = _find_closest_valid_position(starting_point, map_tiles)
     locations = [closest_valid_starting_point]
@@ -82,51 +83,63 @@ def place_locations(map_tiles: ndarray) -> ndarray:
     max_angle_iterations = 360
     angle_increment = 2.0 * math.pi / max_angle_iterations
 
+    # Keep generating locations as long as we have seeds to use
     while len(seed_locations) > 0:
+        # Pick a random seed location to use as a start point
         seed_location = random.choice(tuple(seed_locations))
+        seed_x = seed_location[0]
+        seed_y = seed_location[1]
 
-        placed_new_location = False
+        # Start at a random angle and progressively add to the angle until we find a location that is valid or we run
+        # out of attempts.
         angle = 2.0 * math.pi * random.random()
+        placed_new_location = False
         for k in range(max_angle_iterations):
             angle = (angle + angle_increment) % (2.0 * math.pi)
-            new_location_x = int(seed_location[0] + math.cos(angle) * distance_to_new_city)
-            new_location_y = int(seed_location[1] - math.sin(angle) * distance_to_new_city)
+            new_location_x = int(seed_x + math.cos(angle) * distance_to_new_city)
+            new_location_y = int(seed_y - math.sin(angle) * distance_to_new_city)
             new_location = (new_location_x, new_location_y)
 
             if _location_is_valid(new_location, locations, map_tiles, minimum_distance_between_cities):
                 locations.append(new_location)
                 seed_locations.add(new_location)
                 placed_new_location = True
+                # We found a valid location so we don't need to continue adding to the angle
                 break
 
+        # If we did not find a valid location then this seed location is no longer able to place new locations.
+        # Remove it from the seed set so we don't try to use it again.
         if not placed_new_location:
             seed_locations.remove(seed_location)
 
-    for location in locations:
-        map_tiles_with_cities[location[0]][location[1]] = 99
-        map_tiles_with_cities[location[0]][location[1] + 1] = 99
-        map_tiles_with_cities[location[0]][location[1] - 1] = 99
-        map_tiles_with_cities[location[0] + 1][location[1]] = 99
-        map_tiles_with_cities[location[0] - 1][location[1]] = 99
+    LOG.info(f"Locations placed: {len(locations)}")
 
-    print(f"Cities: {len(locations)}")
-
-    return map_tiles_with_cities
+    return locations
 
 
 def main():
     size = 1024
     map_tiles = generate_map(size, size)
-    map_tiles_with_cities = place_locations(map_tiles)
 
-    rgb_value_matrix = numpy.zeros(map_tiles_with_cities.shape + (3,))
+    LOG.info("Calculating locations...")
+    locations = calculate_locations(map_tiles)
+
+    for location in locations:
+        map_tiles[location[0]][location[1]] = 99
+        map_tiles[location[0]][location[1] + 1] = 99
+        map_tiles[location[0]][location[1] - 1] = 99
+        map_tiles[location[0] + 1][location[1]] = 99
+        map_tiles[location[0] - 1][location[1]] = 99
+
+    rgb_value_matrix = numpy.zeros(map_tiles.shape + (3,))
     for x in range(rgb_value_matrix.shape[0]):
         for y in range(rgb_value_matrix.shape[1]):
-            rgb_value_matrix[x][y] = MapTile.get_rgb_value(map_tiles_with_cities[x][y])
+            rgb_value_matrix[x][y] = MapTile.get_rgb_value(map_tiles[x][y])
 
     image = Image.fromarray(rgb_value_matrix.astype("uint8"), "RGB")
     image.show()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
