0733323ff7e16e9c02e73223a760c537444f63a4
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-22 15:32:37 -0500

    Expand map size in unit test

--- citygame/tests/util/test_maps.py
+++ citygame/tests/util/test_maps.py
@@ -5,7 +5,8 @@ from citygame.src.util.maps import generate_map
 
 class TestMaps:
     def test_generate_map(self):
-        map_object = generate_map(10, 10)
+        map_size = 100
+        map_object = generate_map(map_size, map_size)
 
         assert not numpy.isnan(map_object).any()
         assert not numpy.isinf(map_object).any()
