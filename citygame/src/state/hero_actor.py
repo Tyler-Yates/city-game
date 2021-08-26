fa744eb07774f46c13575655f77950b394309ab6
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-25 22:12:13 -0500

    Add hero actor

---
+++ citygame/src/state/hero_actor.py
@@ -0,0 +1,47 @@
+import uuid
+
+from pygame import Color
+from pygame.surface import Surface
+
+from citygame.src.interfaces.actor import Actor
+from citygame.src.state.location_actor import LocationActor
+from citygame.src.util.fonts import BASIC_FONT, render_font_center
+
+
+class Hero(Actor):
+    """
+    Representation of a hero.
+    """
+
+    def __init__(self, current_location: LocationActor):
+        super().__init__()
+
+        self.id = str(uuid.uuid4())
+        self.name = self.id
+        self.level = 1
+
+        self.current_location = current_location
+
+    def process_input(self, events):
+        pass
+
+    def update(self, time_delta: float):
+        pass
+
+    def render(self, surface: Surface):
+        render_font_center(surface, f"{self.name} Lv. {self.level}", 14, Color("white"))
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.id == other.id
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return f"({self.name} - Lv. {self.level}"
+
+    def __repr__(self):
+        return self.__str__()
