445bb9b37210e47390e60a8cd5478f75013fcebd
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2021-08-28 21:11:00 -0500

    Default map size as a constant

--- citygame/src/state/game_state.py
+++ citygame/src/state/game_state.py
@@ -1,3 +1,4 @@
+from citygame.src.constants.world_constants import DEFAULT_MAP_SIZE
 from citygame.src.state.world_state import WorldState
 
 
@@ -6,7 +7,7 @@ class GameState:
     Class meant to hold the entire game state.
     """
 
-    def __init__(self, map_size: int = 300):
+    def __init__(self, map_size: int = DEFAULT_MAP_SIZE):
         self.map_size = map_size
 
         # The world state is generated async by the world creation scene
